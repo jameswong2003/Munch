792d55f5d10f5e218bfd12a58f3c713651c7d91a
bui-jeremy <jeremybui2002@gmail.com>
2024-03-23 15:20:51 -0400

    Added c# code to show how reservation functionality would look like

---
+++ Munch/ReservationAPI/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ReservationAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.tBoxName = new System.Windows.Forms.TextBox();
+            this.tboxLocation = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.btnCreateRes = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblTIme = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(15, 385);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(137, 53);
+            this.btnLoadData.TabIndex = 0;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnCreateReservation_Click);
+            // 
+            // tBoxName
+            // 
+            this.tBoxName.Location = new System.Drawing.Point(145, 52);
+            this.tBoxName.Name = "tBoxName";
+            this.tBoxName.Size = new System.Drawing.Size(227, 22);
+            this.tBoxName.TabIndex = 1;
+            // 
+            // tboxLocation
+            // 
+            this.tboxLocation.Location = new System.Drawing.Point(145, 90);
+            this.tboxLocation.Name = "tboxLocation";
+            this.tboxLocation.Size = new System.Drawing.Size(227, 22);
+            this.tboxLocation.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 55);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(112, 16);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Restaurant Name";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(12, 95);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(58, 16);
+            this.lblLocation.TabIndex = 5;
+            this.lblLocation.Text = "Location";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(12, 134);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(78, 16);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "Date of Visit";
+            // 
+            // btnCreateRes
+            // 
+            this.btnCreateRes.Location = new System.Drawing.Point(15, 219);
+            this.btnCreateRes.Name = "btnCreateRes";
+            this.btnCreateRes.Size = new System.Drawing.Size(153, 23);
+            this.btnCreateRes.TabIndex = 9;
+            this.btnCreateRes.Text = "Create Reservation";
+            this.btnCreateRes.UseVisualStyleBackColor = true;
+            this.btnCreateRes.Click += new System.EventHandler(this.btnCreateRes_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblTitle.Location = new System.Drawing.Point(10, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(195, 29);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "Reservation API";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(145, 129);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // lblTIme
+            // 
+            this.lblTIme.AutoSize = true;
+            this.lblTIme.Location = new System.Drawing.Point(12, 173);
+            this.lblTIme.Name = "lblTIme";
+            this.lblTIme.Size = new System.Drawing.Size(80, 16);
+            this.lblTIme.TabIndex = 12;
+            this.lblTIme.Text = "Time of Visit";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(145, 168);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(351, 160);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(113, 43);
+            this.btnFind.TabIndex = 14;
+            this.btnFind.Text = "Find Free Time in Calendar";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.lblTIme);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnCreateRes);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.tboxLocation);
+            this.Controls.Add(this.tBoxName);
+            this.Controls.Add(this.btnLoadData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.TextBox tBoxName;
+        private System.Windows.Forms.TextBox tboxLocation;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button btnCreateRes;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblTIme;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ Munch/ReservationAPI/Form1.cs
@@ -0,0 +1,194 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReservationAPI
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateReservation_Click(object sender, EventArgs e)
+        {
+            string jsonString = "{\"id\": \"mP1EdIafQKMuOm9O4PzAfA\", \"alias\": \"barcelona-wine-bar-south-end-boston-6\", \"name\": \"Barcelona Wine Bar South End\", \"image_url\": \"https://s3-media3.fl.yelpcdn.com/bphoto/5b1kPcvyjRezfN7gB7bJOg/o.jpg\", \"is_claimed\": true, \"is_closed\": false, \"url\": \"https://www.yelp.com/biz/barcelona-wine-bar-south-end-boston-6?adjust_creative=g71Xf3LSsqwyvP75W36_9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=g71Xf3LSsqwyvP75W36_9g\", \"phone\": \"+16172662600\", \"display_phone\": \"(617) 266-2600\", \"review_count\": 1467, \"categories\": [{\"alias\": \"spanish\", \"title\": \"Spanish\"}, {\"alias\": \"wine_bars\", \"title\": \"Wine Bars\"}, {\"alias\": \"tapasmallplates\", \"title\": \"Tapas/Small Plates\"}], \"rating\": 4.4, \"location\": {\"address1\": \"525 Tremont St\", \"address2\": null, \"address3\": \"\", \"city\": \"Boston\", \"zip_code\": \"02116\", \"country\": \"US\", \"state\": \"MA\", \"display_address\": [\"525 Tremont St\", \"Boston, MA 02116\"], \"cross_streets\": \"Milford St & Dwight St\"}, \"coordinates\": {\"latitude\": 42.3449355147724, \"longitude\": -71.0705436362457}, \"photos\": [], \"hours\": [{\"open\": [{\"is_overnight\": false, \"start\": \"1600\", \"end\": \"2300\", \"day\": 0}, {\"is_overnight\": false, \"start\": \"1600\", \"end\": \"2300\", \"day\": 1}, {\"is_overnight\": false, \"start\": \"1600\", \"end\": \"2300\", \"day\": 2}, {\"is_overnight\": true, \"start\": \"1600\", \"end\": \"0100\", \"day\": 3}, {\"is_overnight\": true, \"start\": \"1400\", \"end\": \"0100\", \"day\": 4}, {\"is_overnight\": true, \"start\": \"1030\", \"end\": \"0100\", \"day\": 5}, {\"is_overnight\": false, \"start\": \"1030\", \"end\": \"2300\", \"day\": 6}], \"hours_type\": \"REGULAR\", \"is_open_now\": true}], \"transactions\": [\"delivery\"]}\r\n";
+            var restaurant = JsonConvert.DeserializeObject<Restaurant>(jsonString);
+            tBoxName.Text = restaurant.Name;
+            tboxLocation.Text = $"{restaurant.Location.Address1}, {restaurant.Location.City}, {restaurant.Location.State}, {restaurant.Location.Zip_Code}";
+        }
+
+        public class Restaurant
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public string Image_Url { get; set; }
+            public Location Location { get; set; }
+            public List<Category> Categories { get; set; }
+            public double Rating { get; set; }
+        }
+
+        public class Location
+        {
+            public string Address1 { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip_Code { get; set; }
+        }
+
+        public class Category
+        {
+            public string Title { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnCreateRes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime startDate = dateTimePicker1.Value.Date + dateTimePicker2.Value.TimeOfDay;
+                DateTime endDate = startDate.AddHours(1); 
+                
+                UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    new ClientSecrets
+                    {
+                        ClientId = "1010263615545-1qe5busakh2ei19r8n51l9ak1586qrug.apps.googleusercontent.com",
+                        ClientSecret = "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",
+                    },
+                    new[] { CalendarService.Scope.Calendar },
+                    "user", CancellationToken.None);
+
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Munch",
+                });
+
+                Event newEvent = new Event()
+                {
+                    Summary = tBoxName.Text, 
+                    Location = tboxLocation.Text,
+                    Start = new EventDateTime()
+                    {
+                        DateTime = startDate,
+                        TimeZone = "America/New_York" 
+                    },
+                    End = new EventDateTime()
+                    {
+                        DateTime = endDate, 
+                        TimeZone = "America/New_York" 
+                    },
+                   
+                };
+                EventsResource.InsertRequest request = service.Events.Insert(newEvent, "primary"); // "primary" calendar
+                Event createdEvent = await request.ExecuteAsync();
+
+                MessageBox.Show("Event Created: " + createdEvent.HtmlLink);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private async void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                     new ClientSecrets
+                     {
+                         ClientId = "1010263615545-1qe5busakh2ei19r8n51l9ak1586qrug.apps.googleusercontent.com",
+                         ClientSecret = "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",
+                     },
+                     new[] { CalendarService.Scope.Calendar },
+                     "user", CancellationToken.None);
+
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Munch",
+                });
+
+                DateTime selectedDate = dateTimePicker1.Value.Date;
+                DateTime startOfDay = selectedDate;
+                DateTime endOfDay = selectedDate.AddHours(22); // 10 PM
+
+                EventsResource.ListRequest request = service.Events.List("primary");
+                request.TimeMin = startOfDay;
+                request.TimeMax = endOfDay;
+                request.SingleEvents = true;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+                Events events = await request.ExecuteAsync();
+
+                TimeSpan slotDuration = TimeSpan.FromHours(1);
+                DateTime? freeSlotStart = FindFreeSlot(startOfDay, events.Items, slotDuration, endOfDay);
+
+                if (freeSlotStart.HasValue)
+                {
+                    dateTimePicker2.Value = freeSlotStart.Value;
+                }
+                else
+                {
+                    MessageBox.Show("No free slot available today.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private DateTime? FindFreeSlot(DateTime startOfDay, IList<Event> events, TimeSpan slotDuration, DateTime endOfDay)
+        {
+            DateTime currentStart = startOfDay > DateTime.Now ? startOfDay : DateTime.Now;
+
+            foreach (var eventItem in events)
+            {
+                DateTime eventStart = DateTime.Parse(eventItem.Start.DateTimeRaw);
+                DateTime eventEnd = DateTime.Parse(eventItem.End.DateTimeRaw);
+
+                if (currentStart.Add(slotDuration) <= eventStart)
+                {
+                    if (currentStart.Add(slotDuration) <= endOfDay)
+                    {
+                        return currentStart;
+                    }
+                    break;
+                }
+
+                if (eventEnd > currentStart)
+                {
+                    currentStart = eventEnd;
+                }
+            }
+
+            if (currentStart.Add(slotDuration) <= endOfDay)
+            {
+                return currentStart;
+            }
+
+            return null;
+        }
+
+    }
+}
