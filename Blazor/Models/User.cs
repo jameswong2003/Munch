f748899c40efced5029242220925b94abf674a59
bui-jeremy <jeremybui2002@gmail.com>
2024-04-23 01:48:24 -0400

    feat: update UI for contact, trending, and add features to DB

--- Blazor/Models/User.cs
+++ Blazor/Models/User.cs
@@ -1,9 +1,8 @@
-using client2.Pages;
 using Google.Cloud.Firestore;
+using System.Collections.Generic;
 
 [FirestoreData]
-public class User: IFirestoreEntity {
-
+public class User : IFirestoreEntity {
     [FirestoreProperty]
     public string Id { get; set; }
 
@@ -17,22 +16,28 @@ public class User: IFirestoreEntity {
     public string UserName { get; set; }
 
     [FirestoreProperty]
-    public List<Restaurant> LikedRestaurants {get; set;}
+    public List<Restaurant> LikedRestaurants { get; set; }
+
+    [FirestoreProperty]
+    public Dictionary<string, int> CategoryCounts { get; set; } // Tracks category interactions
 
     public User() {
         Id = "";
         Name = "";
         Email = "";
         UserName = "";
-        LikedRestaurants = new List<Restaurant>{};
+        LikedRestaurants = new List<Restaurant>();
+        CategoryCounts = new Dictionary<string, int>();
     }
 
+    // Adding this constructor to match the signup parameters
     public User(string id, string name, string email, string userName) {
         Id = id;
         Name = name;
         Email = email;
         UserName = userName;
         LikedRestaurants = new List<Restaurant>();
+        CategoryCounts = new Dictionary<string, int>();
     }
 
     public void AddRestaurant(Restaurant res) {
